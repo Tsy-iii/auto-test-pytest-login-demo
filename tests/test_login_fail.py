f915a203ab090b114194c836e2272ff31d502c3f
Tsy-iii <306888470@qq.com>
2025-06-22 14:33:02 +0800

    修复登录失败用例的预期提示信息

--- tests/test_login_fail.py
+++ tests/test_login_fail.py
@@ -1,40 +1,34 @@
-# tests/test_login_fail.py
 import pytest
 from pages.login_page import LoginPage
 
-"""这里是数据驱动的多条用例，可以传入多个参数，并自动生成测试用例"""
+# 数据驱动测试用例
 @pytest.mark.parametrize("username, password, expected_msg", [
-    ("", "password123", "Username cannot be empty"),
-    ("admin", "", "Password cannot be empty"),
-    ("wronguser", "wrongpass", "Invalid username or password"),
-    ("locked_out_user", "secret_sauce", "Sorry, this user has been locked out."),
+    ("", "password123", "Epic sadface: Username is required"),
+    ("admin", "", "Epic sadface: Password is required"),
+    ("wronguser", "wrongpass", "Epic sadface: Username and password do not match any user in this service"),
+    ("locked_out_user", "secret_sauce", "Epic sadface: Sorry, this user has been locked out."),
 ])
 def test_login_fail(driver, username, password, expected_msg):
     page = LoginPage(driver)
     page.load()
     page.login(username, password)
-    # assert page.get_error_message() == expected_msg
-    assert expected_msg in page.get_error_msg()
-    
-"""这里是一条一条的测试用例，测试登录失败的情况"""
+    assert expected_msg in page.get_error_message()
+
+# 单条失败用例测试
 def test_login_fail_credentials(driver):
     page = LoginPage(driver)
     page.load()
     page.login("wrong_user", "wrong_pass")
-    # assert "inventory" not in driver.current_url
-    assert "Invalid username and password" in page.get_error_message()
-    
+    assert "Epic sadface: Username and password do not match any user in this service" in page.get_error_message()
+
 def test_login_fail_empty_password(driver):
     page = LoginPage(driver)
     page.load()
     page.login("wrong_user", "")
-    # assert "inventory" not in driver.current_url
-    assert page.get_error_message() == "Please enter a password"
-    
+    assert page.get_error_message() == "Epic sadface: Password is required"
+
 def test_login_fail_empyt_username(driver):
     page = LoginPage(driver)
     page.load()
     page.login("", "wrong_pass")
-    # assert "inventory" not in driver.current_url
-    assert page.get_error_message() == "Please enter a username"
-    
+    assert page.get_error_message() == "Epic sadface: Username is required"
